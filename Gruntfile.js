15e0d8661ec69e2bbf7454e22a0ed52c24c1e427
João M <joaom182@hotmail.com>
2014-09-27 23:23:56 -0300

    Added Travis Task

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+'use strict';
+module.exports = function (grunt) {
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc'
+            },
+            files: [
+                'src/*.js'
+            ]
+        },
+        watch: {
+            files: ['<%= jshint.files %>'],
+            tasks: ['jshint']
+        }
+    });
+
+    grunt.registerTask('travis', ['jshint']);
+    grunt.registerTask('default', 'jshint');
+};
