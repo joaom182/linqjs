3b8ca45316d739ceccf486f00693d8b9765b3c6c
João M <joaom182@hotmail.com>
2014-09-27 23:29:45 -0300

    JS Hint

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function (grunt) {
                 jshintrc: '.jshintrc'
             },
             files: [
-                'src/*.js'
+                'linq.js'
             ]
         },
         watch: {
