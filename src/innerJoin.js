213cdf62510263c1de13cc2b400489bbd90f3676
Aluan Haddad <aluanh@gmail.com>
2016-06-25 22:34:20 -0400

    Validate arguments, make resultSelector optional, only apply key selectors once
    Validate arguments, make resultSelector optional, only apply key selectors once.

--- src/innerJoin.js
+++ src/innerJoin.js
@@ -1,20 +1,30 @@
 (function () {
 	'use strict';
-
-	Array.prototype.innerJoin = function (arr, outer, inner, result, comparer) {
+	
+	Array.prototype.innerJoin = function (inner, outerKeySelector, innerKeySelector, resultSelector, comparer) {
+		if (!inner) { throw new TypeError("inner is undefined."); }
+		if (!innerKeySelector) { throw new TypeError("innerKeySelector is undefined."); }
+		if (!outerKeySelector) { throw new TypeError("outerKeySelector is undefined."); }
+		
+		resultSelector = resultSelector || function (o, i) { 
+			return [o, i];
+		};
 		comparer = comparer || linq.EqualityComparer;
-		var res = [];
-
-		this.forEach(function (t) {
-			arr.where(function (u) {
-					return comparer(outer(t), inner(u));
-				})
-				.forEach(function (u) {
-					res.push(result(t, u));
-				});
+		var innerKeyed = inner.select(function (e) { 
+			return {
+				key: innerKeySelector(e),
+				element: e
+			};
 		});
-
-		return res;
+		return this.selectMany(function (o) { 
+			var key = outerKeySelector(o);
+			return innerKeyed.where(function (i) {
+					return comparer(key, i.key);
+				})
+				.select(function (i) {
+					return i.element;
+				});
+		}, resultSelector.bind(null));
 	};
 
 }());
