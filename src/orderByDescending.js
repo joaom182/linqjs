8ace5f5c9c2baef53b4ea834373d8d5b4da1916e
Wei.Chen <sh.chenwei@foxmail.com>
2019-01-08 13:09:57 +0800

    Update orderByDescending.js
    have bug

--- src/orderByDescending.js
+++ src/orderByDescending.js
@@ -3,7 +3,7 @@
 
 	Array.prototype.orderByDescending = function (selector, comparer) {
 		comparer = comparer || linq.SortComparer;
-		return this.orderBy(linq.Selector, function (a, b) {
+		return this.orderBy(selector||linq.Selector, function (a, b) {
 			return -comparer(a, b);
 		});
 	};
